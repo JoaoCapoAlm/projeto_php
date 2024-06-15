8f1a0164bee12046acb44694175f887e8b522f98
Lucas Levorato <lucaslevorato@gmail.com>
2024-06-15 15:28:56 -0300

    aula

--- menu.php
+++ menu.php
@@ -1,8 +1,19 @@
 <?php
 
+
 $paginaApenasLogado = true;
 include_once './parts/header.php';
-$selectedPeriodo = empty($_POST['periodo']) ? '7' : $_POST['periodo'];
+if (!isset($_SESSION['periodo'])) {
+    $_SESSION['periodo'] = '7';
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['periodo'])) {
+        $_SESSION['periodo'] = $_POST['periodo'];
+    }
+}$selectedCurrency = empty($_GET['currency']) ? 'USD-BRL' : $_GET['currency'];
+
+$selectedPeriodo = $_SESSION['periodo'];
 if(!$_SESSION['user_id']){
     header('Location: ./index.php');
     exit();
@@ -11,19 +22,19 @@ if(!$_SESSION['user_id']){
     <div class="container">
         <form method="post">
             <select name="periodo" id="">
-                <option value=""><?php echo $selectedPeriodo?> dias</option>
-                <option value="7">semana</option>
-                <option value="15">quinzeza</option>
-                <option value="30">mes</option>
-                <option value="90">trimestre</option>
-                <option value="180">semestre</option>
-                <option value="365">ano</option>
+                <option value=""><?php echo $_SESSION['periodo']?> dias</option>
+                <option value="7">7 dias</option>
+                <option value="15">15 dias</option>
+                <option value="30">30 dias</option>
+                <option value="90">90 dias</option>
+                <option value="180">180 dias</option>
+                <option value="365">365 dias</option>
             </select>
             <button type="submit">Aplicar</button>
         </form>
         <?php
 
-        $selectedCurrency = empty($_GET['currency']) ? 'USD-BRL' : $_GET['currency'];
+        
         
         $apiUrl = "https://economia.awesomeapi.com.br/json/daily/$selectedCurrency/$selectedPeriodo";
         generateChartFromApi($apiUrl, 'myChart', 'line',$selectedCurrency, 'Data', 'Valor em Reais');
