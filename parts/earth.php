4c08f16333f35f54ee525b04dbc22842834b6115
Lucas Levorato <lucaslevorato@gmail.com>
2024-06-18 12:53:07 -0300

    aula

--- parts/earth.php
+++ parts/earth.php
@@ -63,7 +63,7 @@
 
 
             const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
-            directionalLight.position.set(-200, 0, 100);
+            directionalLight.position.set(-200, 100, 100);
             scene.add(directionalLight);
 
 
